a3cc024df16ad0c289c59bc11ac2b305add4f64e
soomin <tnals2199@naver.com>
2024-03-30 22:12:42 +0900

    모바일 완료

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,4 +1,4 @@
-import { React, useEffect, useState, useRef } from 'react'
+import { React, useState } from 'react'
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFaceSmile } from '@fortawesome/free-regular-svg-icons'
@@ -6,10 +6,8 @@ import { faSearch, faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Navbar = ({authenticate, setAuthenticate}) => {
   const menuList = ['여성','Divided','남성','신생아/유아','아동','H&M Home','Sale','지속가능성'];
-  const [isMobile, setIsMobile] = useState(false);
-  const [showMenu, setShowMenu] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
   const navigate = useNavigate();
-  const menuRef = useRef();
 
   // const goToLogin = () => {
   //   navigate('/login');
@@ -26,29 +24,6 @@ const Navbar = ({authenticate, setAuthenticate}) => {
   const resetValue = (event) => {
     event.target.value = "";
   }
-  const toggleMenu = () => {
-    setShowMenu(!showMenu); 
-  };
-  const handleOutsideClick = (event) => {
-    if (menuRef.current && !menuRef.current.contains(event.target)) {
-      setShowMenu(false);
-    }
-  };
-
-  useEffect(() => {
-    const handleMobile = () => {
-      setIsMobile(window.innerWidth < 768);
-    }
-
-    handleMobile();
-    window.addEventListener('resize', handleMobile);
-    document.addEventListener('mousedown', handleOutsideClick);
-
-    return () => {
-      window.removeEventListener('resize', handleMobile);
-      document.addEventListener('mousedown', handleOutsideClick);
-    };
-  }, []);
 
   return (
     <div>
@@ -71,18 +46,14 @@ const Navbar = ({authenticate, setAuthenticate}) => {
             <img width={150} src="https://upload.wikimedia.org/wikipedia/commons/5/53/H%26M-Logo.svg"/>
         </div>
         <div className='menu-area'>
-            <div ref={menuRef}>
-              <ul className={`menu-list ${showMenu && isMobile ? 'show' : ''}`}>
+              <ul className={`menu-list ${menuOpen ? "open" : ""}`}>
                 {menuList.map((menu, index) => (
                   <li key={index}>{menu}</li>
                 ))}
               </ul>
+            <div className='hamburger' onClick={() => {setMenuOpen(!menuOpen);}}>
+              <FontAwesomeIcon icon={faBars}/>
             </div>
-          {isMobile && (
-            <div className='hamburger' onClick={toggleMenu}>
-              <FontAwesomeIcon icon={faBars} style={{ display: showMenu ? 'none' : 'block' }}/>
-            </div>
-          )}
         <div className='search-area'>
           <FontAwesomeIcon icon={faSearch} />
           <input type='text'placeholder='검색' onClick={resetValue} onKeyDown={(event) => search(event)}/>
